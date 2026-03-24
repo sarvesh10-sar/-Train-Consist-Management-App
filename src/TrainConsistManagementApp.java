2f6d100be8d53835827fc3d85ec2f7ea36c104a4
sarvesh10-sar <ss7303@srmist.edu.in>
2026-03-24 09:43:08 +0530

    UC3:  Centralized Room Inventory Management

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class TrainConsistManagementApp{
+public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
